d5bb136696204b38469360ac8f01d30c3d6260c7
Jacob Lipina <jrlwd5@mst.edu>
2019-04-16 19:42:28 -0500

    Servo 1 Not Working on Tool End Effector

--- MultitoolEndEffector_Testing/MultitoolEndEffector_Testing.h
+++ MultitoolEndEffector_Testing/MultitoolEndEffector_Testing.h
@@ -9,20 +9,27 @@
 RoveCommEthernetUdp RoveComm;
 
 	//Pinout
-#define SERVO_1_CRTL_PIN	PM_7
-#define SERVO_2_CRTL_PIN	PA_7
-#define SERVO_3_CRTL_PIN	PM_6
-#define LASER_CTR_PIN		PN_3
+#define SERVO_1_CRTL_PIN   PM_7
+#define SERVO_2_CRTL_PIN   PA_7
+#define SERVO_3_CRTL_PIN   PM_6
+#define LASER_CRTL_PIN		PN_3
+#define SOLENOID_CRTL_PIN 	PP_2
+#define SW1_IND_PIN			PH_0
+#define SW2_IND_PIN			PH_1
+#define SW3_IND_PIN			PK_6
+#define SWE_IND_PIN			PK_7
     //Delays
-#define ROVECOMM_DELAY     	5
+#define ROVECOMM_DELAY     	10
 	//Servo Positions
-#define SERVO_1_RETRACTED	180
+#define SERVO_1_RETRACTED	155
 #define SERVO_2_RETRACTED	175
 #define SERVO_3_RETRACTED	155
 #define SERVO_1_SELECTED	10
 #define SERVO_2_SELECTED	10
 #define SERVO_3_SELECTED	10
-
+#define SERVO_1_REST       1470 //1470 is boundary in which it stops drifting to right; 1476 is boundary in which it stops drifting left; 1473 is middle, no drifting
+#define SERVO_2_REST       1400 //Main cam pitch facing up. Can lower value to face further upwards.
+#define SERVO_3_REST       1830 //Drive cam pan facing forward.
 void setup();
 void loop();
 
--- MultitoolEndEffector_Testing/MultitoolEndEffector_Testing.ino
+++ MultitoolEndEffector_Testing/MultitoolEndEffector_Testing.ino
@@ -4,30 +4,36 @@
 #include "RoveComm.h"
 #include <Energia.h>
 
-#define SOLENOID_PIN PP_2
 
 Servo Servos[3];
 
 void setup()
 {
 	Serial.begin(9600);
+  delay(ROVECOMM_DELAY);
 
 	RoveComm.begin(RC_ARMBOARD_FOURTHOCTET);
   delay(ROVECOMM_DELAY);
 
-  pinMode(LASER_CTR_PIN, OUTPUT);
+  //pinMode(LASER_CRTL_PIN, OUTPUT);
+  pinMode(SOLENOID_CRTL_PIN, OUTPUT);
+  pinMode(SW1_IND_PIN, OUTPUT);
+  pinMode(SW2_IND_PIN, OUTPUT);
+  pinMode(SW3_IND_PIN, OUTPUT);
+  pinMode(SWE_IND_PIN, OUTPUT);
 
-  digitalWrite(LASER_CTR_PIN, HIGH);
-
-  pinMode(SOLENOID_PIN, OUTPUT);
-
-  digitalWrite(SOLENOID_PIN, HIGH);
+  //digitalWrite(LASER_CRTL_PIN, HIGH);
+  digitalWrite(SOLENOID_CRTL_PIN, LOW);
+  digitalWrite(SW1_IND_PIN, LOW);
+  digitalWrite(SW2_IND_PIN, LOW);
+  digitalWrite(SW3_IND_PIN, LOW);
+  digitalWrite(SWE_IND_PIN, LOW);
 
   Servos[0].attach(SERVO_1_CRTL_PIN);
   Servos[1].attach(SERVO_2_CRTL_PIN);
   Servos[2].attach(SERVO_3_CRTL_PIN);
 
-  Servos[0].write(SERVO_1_RETRACTED);
+  Servos[0].write(SERVO_1_RETRACTED); //tried Servos[0].writeMicroseconds(1500); and still knothing on PM_7 pin.
   Servos[1].write(SERVO_2_RETRACTED);
   Servos[2].write(SERVO_3_RETRACTED);
 
@@ -36,12 +42,13 @@ void setup()
 
 void loop()
 {
+  //Servos[0].write(SERVO_1_RETRACTED);
 	char incomingByte;
   	if(Serial.available() > 0)
   	{
     	incomingByte = Serial.read();
     	Serial.print(incomingByte);
-    	switch (incomingByte)                    ///(data_id) //either 0 or 1 
+    	switch (incomingByte)   
     	{   
         	//Don't do anything for data_id zero 
         	case '1': //No tool selected
@@ -51,16 +58,34 @@ void loop()
   				  Servos[2].write(SERVO_3_RETRACTED);
         	  	break;
           case '2':
+            Serial.println("Typing tool selected");
+            Servos[0].write(SERVO_1_SELECTED);
+            Servos[1].write(SERVO_2_RETRACTED);
+            Servos[2].write(SERVO_3_RETRACTED);
             break;
           case '3':
+            Serial.println("Hex tool selected");
+            Servos[0].write(SERVO_1_RETRACTED);
+            Servos[1].write(SERVO_2_SELECTED);
+            Servos[2].write(SERVO_3_RETRACTED);
             break;
           case '4':
+            Serial.println("Screwdriver tool selected");
+            Servos[0].write(SERVO_1_RETRACTED);
+            Servos[1].write(SERVO_2_RETRACTED);
+            Servos[2].write(SERVO_3_SELECTED);
             break;
           case '5':
-            digitalWrite(LASER_CTR_PIN, HIGH);
+            //digitalWrite(LASER_CRTL_PIN, LOW);
+            digitalWrite(SOLENOID_CRTL_PIN, LOW); //Solenoid off
             break;
           case '6': 
-            digitalWrite(LASER_CTR_PIN, LOW);
+            //digitalWrite(LASER_CRTL_PIN, HIGH);
+            digitalWrite(SOLENOID_CRTL_PIN, HIGH); //Solenoid actuates
+            digitalWrite(SW1_IND_PIN, HIGH);
+            delay(250);
+            digitalWrite(SOLENOID_CRTL_PIN, LOW); //Solenoid actuates
+            digitalWrite(SW1_IND_PIN, LOW);
             break;
       }
     }
